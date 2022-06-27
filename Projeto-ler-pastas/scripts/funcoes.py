3c7bb69fecec9d949626ba46184eb36f38daf158
Pedro <peedinho1930@gmail.com>
2022-06-27 18:33:04 -0300

    --

--- Projeto-ler-pastas/scripts/funcoes.py
+++ Projeto-ler-pastas/scripts/funcoes.py
@@ -6,17 +6,19 @@ import numpy as np
 
 
 def cria_arquivos(tamanho):
-    lista_dataframes=[]
+    lista_df=[]
     for i in range(0,tamanho):
-        lista_dataframes.append(pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=['coluna_1','coluna_2','coluna_3','coluna_4']))
-    return lista_dataframes
+        lista_df.append(pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=['coluna_1','coluna_2','coluna_3','coluna_4']))
+    return lista_df
 
-def salva_arquivos(lista_dataframes,caminho):
-    for i in range(0,len(lista_dataframes)):
+def salva_arquivos(lista_df,caminho):
+    for i in range(0,len(lista_df)):
         pd.to_csv(f'{caminho}\df{i}.csv')
 
-def junta_arquivos(caminho):
+def junta_arquivos(caminho,lista_df):
     aux=pd.DataFrame()
+    for df in lista_df:
+        aux=aux.merge(df)
 
 
     
